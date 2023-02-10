e0e427c1bf6d32503f3eac9fee52b9ebbeccf2fc
adzhigitovruslan <adzhigitovruslan@gmail.com>
2023-02-10 13:50:46 +0800

    feat: add js function

--- src/index.js
+++ src/index.js
@@ -1,46 +1,66 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  '.-': 'a',
+  '-...': 'b',
+  '-.-.': 'c',
+  '-..': 'd',
+  '.': 'e',
+  '..-.': 'f',
+  '--.': 'g',
+  '....': 'h',
+  '..': 'i',
+  '.---': 'j',
+  '-.-': 'k',
+  '.-..': 'l',
+  '--': 'm',
+  '-.': 'n',
+  '---': 'o',
+  '.--.': 'p',
+  '--.-': 'q',
+  '.-.': 'r',
+  '...': 's',
+  '-': 't',
+  '..-': 'u',
+  '...-': 'v',
+  '.--': 'w',
+  '-..-': 'x',
+  '-.--': 'y',
+  '--..': 'z',
+  '.----': '1',
+  '..---': '2',
+  '...--': '3',
+  '....-': '4',
+  '.....': '5',
+  '-....': '6',
+  '--...': '7',
+  '---..': '8',
+  '----.': '9',
+  '-----': '0',
 };
 
 function decode(expr) {
-    // write your solution here
+  let dividedByTen = expr.match(/.{1,10}/g);
+
+  let numbersToSymbols = []
+  for (let i = 0; i < dividedByTen.length; i++) {
+    let index = [...dividedByTen[i]].findIndex(el => el == 1);
+    if (index !== -1) {
+      numbersToSymbols.push(dividedByTen[i].slice(index).replaceAll('11', '-').replaceAll('10', '.'));
+    } else {
+      numbersToSymbols.push(dividedByTen[i]);
+    }
+  }
+
+  let decodedStr = '';
+  for (let i = 0; i < numbersToSymbols.length; i++) {
+    if (numbersToSymbols[i] == '**********') {
+      decodedStr += ' ';
+    } else {
+      decodedStr += MORSE_TABLE[numbersToSymbols[i]];
+    }
+  }
+  return decodedStr
 }
 
 module.exports = {
-    decode
+  decode
 }
